6beaa52b5d1eb478a785c11dbc2274d7af6a013c
Lars Ivar Hatledal <laht@ntnu.no>
2025-02-13 10:55:11 +0100

    remove minimumCost

--- include/pathfinding/heuristics/ManhattanHeuristic.hpp
+++ include/pathfinding/heuristics/ManhattanHeuristic.hpp
@@ -15,15 +15,11 @@ namespace pathfinding {
     class ManhattanHeuristic: public Heuristic {
 
     public:
-        explicit ManhattanHeuristic(float minimumCost): minimumCost_(minimumCost) {}
-
         float getCost(const TileBasedMap& map, const Coordinate& start, const Coordinate& target) override {
-            return minimumCost_ * static_cast<float>(std::abs(start.x - target.x) + std::abs(start.y - target.y));
+            return static_cast<float>(std::abs(start.x - target.x) + std::abs(start.y - target.y));
         }
-
-    private:
-        float minimumCost_;
     };
+
 }// namespace pathfinding
 
 #endif//PATHFINDING_MANHATTANHEURISTIC_HPP
